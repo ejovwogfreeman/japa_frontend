efe4a4fbe440ecfd0df388b2b81f163e36f9ebeb
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-10-07 15:15:06 +0100

    message

--- src/pages/AddModal.jsx
+++ src/pages/AddModal.jsx
@@ -44,7 +44,7 @@ const AddModal = ({ handleShowAddModal, token }) => {
       console.log(form);
       // Send the form data to your server
       const response = await axios.post(
-        "https://japaconsults.sammykingx.tech/drafts/save",
+        "https://japaconsults.sammykingx.tech/notes/save",
         form,
         {
           headers: {
